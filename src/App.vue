fe8a29f9424618b0ada9f52819729504cec1e281
ashkan20171 <ashkan.mataee@yahoo.com>
2021-12-04 17:13:40 +0330

    add axios

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <img src="./assets/city.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <div class="input-group mb-3">
-  <button class="btn btn-primary" type="button" id="button-addon1">ارسال</button>
+  <button class="btn btn-primary" type="button" @click="getData" id="button-addon1">ارسال</button>
   <input type="text" class="form-control" placeholder="اسم شهر را وارد کنید" aria-label="Example text with button addon" aria-describedby="button-addon1">
      </div>
     <p class="card-text"></p>
@@ -14,6 +14,22 @@
 </template>
 
 <script>
+const API_KEY = 'ec0df541ed9c9cd210bb42bd99cf4a1e';
+
+export default {
+name: "App",
+
+data: () => ({
+
+}),
+
+methods: {
+ getData() {
+ 
+ } 
+}
+
+}
 </script>
 
 <style>
